9da361a2799ec67c0e32ee353a2adcc15da953c7
Alex Kir <alex@wall-dev.org>
2015-05-16 10:04:25 +0800

    Initial version

---
+++ README.md
@@ -0,0 +1,23 @@
+# TaskWarrior Report
+
+A supplement script to kostajh/taskwarrior-time-tracking-hook.
+Getting total time for complete tasks on a project.
+
+## Usage
+```
+python report.py -h
+usage: report.py [-h] [-f YYYY-DD-MM] [-t YYYY-DD-MM] project
+
+positional arguments:
+  project
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f YYYY-DD-MM, --from YYYY-DD-MM
+                        From date, inclusive
+  -t YYYY-DD-MM, --to YYYY-DD-MM
+                        To date, exclusive
+```
+
+For ease of use it is possible to `chmod +x report.py` and put it
+in PATH
---
+++ report.py
@@ -0,0 +1,44 @@
+import taskw
+import argparse
+
+
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('project', type=str, metavar="project")
+    parser.add_argument(
+        '-f', '--from', dest='entry.after', type=str, metavar="YYYY-DD-MM",
+        help="From date, inclusive")
+    parser.add_argument(
+        '-t', '--to', type=str, dest='entry.before', metavar="YYYY-DD-MM",
+        help="To date, exclusive")
+    return parser
+
+
+def get_total(tasks):
+    return sum([int(t['totalactivetime'].split('seconds')[0]) for t in tasks])
+
+
+def parse_seconds(sec):
+    m, s = divmod(sec, 60)
+    h, m = divmod(m, 60)
+    return h, m, s
+
+
+def main(args):
+    project = args.project
+    print("Parsing project %s" % project)
+    tw = taskw.TaskWarrior()
+    filter_ = {'status': 'completed'}
+    for k, v in args._get_kwargs():
+        if v:
+            filter_[k] = v
+    tasks = tw.filter_tasks(filter_)
+    total = get_total(tasks)
+    h, m, s = parse_seconds(total)
+    print("Total for project %s is %d:%d:%d" % (project, h, m, s))
+
+
+if __name__ == '__main__':
+    parser = argparser()
+    args = parser.parse_args()
+    main(args)
